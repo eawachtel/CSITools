871c7ef1e5ced3c7836afb8a0412ef88e2c1a645
gwachtel <gene.wachtel@gmail.com>
2021-07-14 17:55:16 -0400

    jackscrew functions added, PM Batch Export working

--- app/components/batch-creation/batch-creation.component.ts
+++ app/components/batch-creation/batch-creation.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { values } from 'lodash';
 
 import * as Papa from 'papaparse';
+import { ExportToCsv } from 'export-to-csv';
 
 @Component({
   selector: 'batch-creation',
@@ -16,6 +18,7 @@ export class BatchCreationComponent implements OnInit {
   excludedValueStrings: string[] = ['Run', 'R1']
   inputs: string[] = [];
   inputDict: any = {};
+  functionInputFactors: any[] = ['JackscrewAdjustLR', 'JackscrewAdjustRR'];
 
   constructor() { }
 
@@ -61,9 +64,35 @@ export class BatchCreationComponent implements OnInit {
         let value = element[designKey];
         let inputKey = this.inputDict[designKey];
         this.batchDict[inputKey]['values'].push(value)
+      });
     });
-  });
-}
+  }
+
+  public async processFunctions() {
+      Object.keys(this.batchDict).forEach((batchKey:any) => {
+        if (this.functionInputFactors.includes(batchKey)) {
+          switch(batchKey) {
+            case 'JackscrewAdjustLR':
+              this.batchDict[batchKey]['values'] = this.jackscrewScale(this.batchDict[batchKey]['values']);
+              break;
+            case 'JackscrewAdjustRR':
+              this.batchDict[batchKey]['values'] = this.jackscrewScale(this.batchDict[batchKey]['values']);
+              break;
+          }
+        }
+      })
+  }
+
+  public jackscrewScale(values: any[]) {
+    let newValues: any[] = []
+    values.forEach((number) => {
+      let newNumber = ((number / 12) * -1).toFixed(3);
+      newValues.push(newNumber);
+    });
+
+    return newValues
+  }
+
 
   public  fileChangeListener(files: any) {
     let file = files.target.files[0];
@@ -76,12 +105,43 @@ export class BatchCreationComponent implements OnInit {
         complete: async (result) => {
           await this.processInputs(result.data); // get input factors in a list and add to batchmatrix as row
           await this.processValues(result.data); // get values and enter into array in batchmatrix
-        }});
+          await this.processFunctions(); // run functions on channels requiring addition channel defs ie. FARB ARMS, Jackscrew ect
+          Object.keys(this.batchDict).forEach((key:any) => {
+            let valuesArr = this.batchDict[key]['values'];
+            let newValues = '';
+            valuesArr.forEach((element:any) => {
+              newValues = newValues + element.toString()+ ', ' 
+            });
+            this.batchDict[key]['values'] = newValues;
+            this.batchMatrix.push(this.batchDict[key])
+          });
+        }
+      });
+      console.log(this.batchMatrix)
     } else {
       alert('Problem loading CSV file');
     }
   }
 
+  exportBatchMatrix(){
+    const options = { 
+      fieldSeparator: ',',
+      filename: 'PM Batch Matrix',
+      quoteStrings: '"',
+      decimalSeparator: '.',
+      showLabels: false, 
+      showTitle: false,
+      title: 'My Awesome CSV',
+      useTextFile: false,
+      useBom: true,
+      useKeysAsHeaders: true,
+      // headers: ['Column 1', 'Column 2', etc...] <-- Won't work with useKeysAsHeaders present!
+    };
+   
+  const csvExporter = new ExportToCsv(options);
+   
+  csvExporter.generateCsv(this.batchMatrix);
+
+  }
+
 }
-
-
